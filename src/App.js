163d72a813c490677bba86eb36b1bc4429d3d066
Joao Miguel Stroher <joao.stroher@gmail.com>
2020-04-19 20:46:56 -0300

    challenge done

--- src/App.js
+++ src/App.js
@@ -1,27 +1,84 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
+import api from "./services/api";
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+  const [title, setTitle] = useState("");
+  const [url, setUrl] = useState("");
+  const [techs, setTechs] = useState("");
+
+  useEffect(() => {
+    async function loadData() {
+      const response = await api.get("repositories");
+      setRepositories(response.data);
+    }
+    loadData();
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post("repositories", {
+      title,
+      url,
+      techs,
+    });
+    setTitle("");
+    setUrl("");
+    setTechs("");
+    setRepositories([...repositories, response.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    console.log(id);
+    await api.delete(`repositories/${id}`);
+    const newRepositories = repositories.filter(
+      (repository) => repository.id !== id
+    );
+    setRepositories(newRepositories);
+  }
+
+  function getValueAndPreventDefault(event) {
+    event.preventDefault();
+    return event.target.value;
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map((repository) => (
+          <li key={String(repository.id)}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
+      <label>
+        Title
+        <input
+          type="text"
+          value={title}
+          onChange={(event) => setTitle(getValueAndPreventDefault(event))}
+        />
+      </label>
+      <label>
+        URL
+        <input
+          type="text"
+          value={url}
+          onChange={(event) => setUrl(getValueAndPreventDefault(event))}
+        />
+      </label>
+      <label>
+        Techs
+        <input
+          type="text"
+          value={techs}
+          onChange={(event) => setTechs(getValueAndPreventDefault(event))}
+        />
+      </label>
 
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
